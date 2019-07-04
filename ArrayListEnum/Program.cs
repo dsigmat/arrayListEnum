1bb2457fa2364083d1ea479d007713fac297e1ae
Denis Sidorov <dsigmat@hotmail.com>
2019-07-04 10:58:01 +0600

    Add project files.

---
+++ ArrayListEnum/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ArrayListEnum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
