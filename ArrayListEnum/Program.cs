e03088d6cd8fe09aea36ec0b387af65cb4158caf
Denis Sidorov <dsigmat@hotmail.com>
2019-07-04 11:25:27 +0600

    list

--- ArrayListEnum/Program.cs
+++ ArrayListEnum/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace ArrayListEnum
 {
@@ -6,9 +7,25 @@ namespace ArrayListEnum
     {
         static void Main(string[] args)
         {
+            #region array
+            //int[] array = new int[10];
+            //array[0] = 20;
+            //array[1] = 12;
+            //array[2] = 11;
+            //array[3] = 21;
+            //array[4] = 1;
+
+            //foreach (var item in array)
+            //{
+            //    Console.WriteLine(item);
+            //}
+            #endregion
+
             int[] array = new int[10];
-            array[0] = 20;
-            Console.WriteLine(array[0]);
+
+            List<int> list = new List<int>();
+
+            Console.ReadLine();
         }
     }
 }
